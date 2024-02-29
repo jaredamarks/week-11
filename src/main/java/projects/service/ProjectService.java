ee4eebb97f41e764d562e4ce99af27deb98fef67
Jared <idku@duck.com>
2024-02-28 17:39:51 -0800

    first commit

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 public class ProjectService {
 		private ProjectDao projectDao = new ProjectDao();
@@ -17,5 +18,15 @@ public class ProjectService {
 		public Project fetchProjectById(Integer projectId) {
 			return projectDao.fetchProjectById(projectId).orElseThrow();
 		}
-	}
+		public void modifyProjectDetails(Project project) {
+			if(!projectDao.modifyProjectDetails(project)) {
+				throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+		}
+		}
+		public void deleteProject(Integer projectId) {
+			if(!projectDao.deleteProject(projectId)) {
+				throw new DbException("Project with ID=" + projectId + " does not exist.");
+			}
+		}
+		}
 
